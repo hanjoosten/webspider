4350815a3fca6b5e7a14c683f1fc0004a3fc7b27
Han Joosten <han.joosten.han@gmail.com>
2017-07-08 02:21:44 +0200

    tot en met links

---
+++ src/Main.hs
@@ -0,0 +1,94 @@
+module Main where
+
+import Data.ByteString.Lazy.UTF8
+import Data.Maybe
+import Network.HTTP.Conduit
+import Network.URI
+import Text.HTML.TagSoup
+
+main :: IO ()
+main = do
+  putStrLn "hello world"
+
+
+download :: String -> IO String
+download url = do res <- simpleHttp url
+                  return (toString res)
+
+saveAs :: String -> Int -> IO ()
+saveAs url k =
+  do content <- download url
+     writeFile (makeFileName k) content
+
+makeFileName :: Int -> FilePath 
+makeFileName k = "download-" ++ show k ++ ".html"
+
+processPage :: String -> IO [String]
+processPage url = do
+  page <- download url
+  return (links url page)
+
+nofollow :: Tag String -> Bool
+nofollow tag = fromAttrib "rel" tag == "nofollow"
+
+--process :: String -> [String]
+process = undefined -- links
+
+links :: String -> String -> [String]
+links url =
+  catMaybes .
+  map (canonicalize url) .
+  filter (not . null) .
+  map (fromAttrib "href") .
+  filter (\t -> fromAttrib "rel" t /= "nofollow") .
+  filter (isTagOpenName "a") .
+  canonicalizeTags .
+  parseTags
+  
+
+canonicalizeLink :: String -> String -> Maybe String
+canonicalizeLink referer path =
+  case parseURI referer of
+    Nothing -> Nothing
+    Just r  ->
+      case parseURIReference path of
+        Nothing -> Nothing
+        Just p  -> Just (uriToString id (nonStrictRelativeTo p r) "")
+
+bind :: Maybe a -> (a -> Maybe b) -> Maybe b
+bind  Nothing      _     = Nothing
+bind (Just value) action = action value
+
+canonicalizeLink1 :: String -> String -> Maybe String
+canonicalizeLink1 referer path =
+  parseURI referer                `bind`
+   \r -> parseURIReference path   `bind`
+    \p -> Just (uriToString id (nonStrictRelativeTo p r) "")
+
+canon :: String -> String -> Maybe String
+canon referer path =
+  parseURI referer         `bind` \r ->
+  parseURIReference path   `bind` \p ->
+  Just (uriToString id (nonStrictRelativeTo p r) "")
+
+canon1 :: String -> String -> Maybe String
+canon1 referer path =
+  parseURI referer >>= \r ->
+  parseURIReference path >>= \p ->
+  Just (uriToString id (nonStrictRelativeTo p r) "")
+
+canonicalize :: String -> String -> Maybe String
+canonicalize referer path = do
+  r <- parseURI referer
+  p <- parseURIReference path
+  return (uriToString id (nonStrictRelativeTo p r) "")
+
+process' :: String -> String -> [Maybe String]
+process' url =
+   map (canonicalize url) .
+   filter (not . null) .
+   map (fromAttrib "href") .
+   filter (\t -> fromAttrib "rel" t /= "nofollow") .
+   filter (isTagOpenName "a") .
+   canonicalizeTags .
+   parseTags
